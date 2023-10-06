8243406d3070e993824af197e1dd2db015143a6c
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-06 22:22:03 +0600

    Deleted Method Implement Successfully

--- src/componenets/Users.jsx
+++ src/componenets/Users.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const usersData = useLoaderData()
-    console.log(usersData);
+    const loadedData = useLoaderData();
+    const [usersData, setUsersData] = useState(loadedData);
+
+
+    // Delete Operation:
+    const handleDelete = user => {
+        const id = user._id;
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    alert(`${user.name} deleted successfully`);
+                    const remainingData = usersData.filter(user => user._id !== id);
+                    setUsersData(remainingData);
+                }
+            })
+    }
     return (
         <div>
             <h3>Users Collection</h3>
             <div className="mt-5">
                 {
-                    usersData.map(user => <div className="p-3 bg-dark text-white m-3 rounded" key={user._id}>
+                    usersData?.map(user => <div className="p-3 bg-dark text-white m-3 rounded" key={user._id}>
                         <h6 className="text-warning">{user.name}😍</h6>
                         <h6>{user.email}✅</h6>
+                        <Button onClick={() => handleDelete(user)} variant="danger mt-2" size="sm">Delete User</Button>
                     </div>)
                 }
             </div>
